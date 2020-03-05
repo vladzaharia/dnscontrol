c92cc65dd73c1c0606123f477e01a842d1994e19
Vlad Zaharia <vlad@zaharia.io>
2020-03-04 19:36:02 -0800

    Update home IP

--- src/services/core.ts
+++ src/services/core.ts
@@ -48,7 +48,7 @@ export const CoreServices: Record[] = [
     { name: 'ba.sea.core', description: 'Barium - Phone', type: 'A', target: '104.37.168.29' },
     { name: 'co.sea.core', description: 'Cobalt - Docker', type: 'A', target: '104.37.168.30' },
     { name: 'ga.sea.core', description: 'Gallium - Mail', type: 'A', target: '104.37.168.31' },
-    { name: 'he.sea.core', description: 'Helium - Home', type: 'A', target: '174.127.231.221' },
+    { name: 'he.sea.core', description: 'Helium - Home', type: 'A', target: '174.127.231.21' },
     { name: 'pd.van.core', description: 'Palladium - Parents', type: 'A', target: '64.46.8.37' },
     { name: 'ir.qnc.core', description: 'Iridium - Azure CloudVM', type: 'A', target: '52.183.82.105' },
     { name: 'li.qnc.core', description: 'Lithium - Azure VPN', type: 'A', target: '52.247.206.58' },
