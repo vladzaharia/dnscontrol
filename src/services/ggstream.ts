678ead000c830b095e8b85004356e5436d18de40
Vlad Zaharia <vlad@zaharia.io>
2020-07-26 21:47:10 -0700

    Add trafficmanager support

--- src/services/ggstream.ts
+++ src/services/ggstream.ts
@@ -3,25 +3,66 @@ import { GetHost, GetIP } from "./core";
 import { ElementNames } from "../utils/server";
 
 /**
- * Current GGStream Instance
+ * UI Host
  */
-const GGStream: ElementNames = 'Manganese';
-const GGStreamHost = GetHost(GGStream);
-const GGStreamIP = GetIP(GGStream);
+const UIServer: ElementNames = 'Manganese';
+const UIHost = GetHost(UIServer);
+const UIIP = GetIP(UIServer);
+
+/**
+ * Service Instances
+ */
+const SvcInstances: GGStreamInstance[] = [
+    {
+        name: "Azure",
+        suffix: "azure",
+        target: GetHost('Manganese')
+
+    },
+];
+
+function GetRecords(): Record[] {
+    return SvcInstances.map((instance) => {
+        return {
+            name: `svc-${instance.suffix}`,
+            description: `GGStream Service - ${instance.name}`,
+            target: instance.target,
+
+        };
+    });
+}
 
 /**
  * External (zaharia.io) Azure Services
  */
 export const GGStreamServices: Record[] = [
-    { name: '@', description: "GGStream Root", target: GGStreamIP, type: 'A', proxy: true },
-    { name: 'www', description: "GGStream www", target: GGStreamHost, proxy: true },
-    { name: 'svc', description: "GGStream Service", target: GGStreamHost },
-    { name: 'svc-azure', description: "GGStream Service - Azure", target: GetHost('Manganese') },
+    { name: '@', description: "GGStream Root", target: UIIP, type: 'A', proxy: true },
+    { name: 'www', description: "GGStream www", target: UIHost, proxy: true },
+    { name: 'svc', description: "GGStream Service - Routed", target: 'ggstream-svc.trafficmanager.net' },
+
+    ... GetRecords(),
 ];
 
 /**
  * GGStream Site Extension
  */
 export const GGStreamSiteExtServices: Record[] = [
-    { name: 'stream', description: "GGStream - Site", target: GGStreamHost, proxy: true }
+    { name: 'stream', description: "GGStream - Site", target: UIHost, proxy: true }
 ];
+
+interface GGStreamInstance {
+    /**
+     * Display name of instance
+     */
+    name: string;
+
+    /** 
+     * Suffix of instance, used for svc-<suffix>.ggstream.app
+     */
+    suffix: string;
+
+    /**
+     * Target of instance
+     */
+    target: string;
+}
