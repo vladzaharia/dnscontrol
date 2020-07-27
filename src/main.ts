d9f21520a843c0ebe3501ce1ae2d8e37337abaf1
Vlad Zaharia <vlad@zaharia.io>
2020-07-26 17:03:57 -0700

    Refactor build commands

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@
  * zhr.one DNS Bootstrapper
  */
 
+console.log('Building zones...\n')
+
 // Get all zone files in zones/
 const context = require.context("./zones/", true, /\.ts$/);
 
 // Import each zone
 context.keys().forEach((zone: string) => {
-    console.log(`\n--\n\nLoading ${zone}...`);
+    console.log(`Loading ${zone}...`);
     context(zone);
+    console.log('\n--\n\n');
 });
 
-console.log('\n--\n');
+console.log('Running dnscontrol...\n');
