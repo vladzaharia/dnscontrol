f300aae0b4183b53d4230968c7699acbcf387bb3
Vlad Zaharia <vlad@zaharia.io>
2020-08-27 01:15:24 -0700

    Update vlad-gg.ts

--- src/zones/vlad-gg.ts
+++ src/zones/vlad-gg.ts
@@ -14,15 +14,11 @@ D('vlad.gg', NoRegistrar, DnsProvider(CloudflareDns),
 
     ... CreateRecords('vlad.gg - GGStream', GGStreamSiteExtServices),
 
-    /* Mailcow records */
-    ... CreateMailcowRecords(GetHost('Gallium'), 
-        '50851205087c610c5172c9a2934e86adacb1fb1e86d9160e212524af51e4cb6e', 
-        'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3TGf0h/Wp/d9Pb6wDH0OdIPSLc7iJVHHQq1oNQ4k3hCX5ntUQ/R33kt9ZQ2kQqmsY+L37WzeIECJfsxeGl2uu//S3V9fugAvj6cHuubhQtcQ8Vwck8V2PsLEUZxFB6eXx9o7AITd+uEL0SmDZ2I4HZm5vhtEZFLL51XY4W1i2mAuACu2QEQFp2f2/bwCJQI0ZRyOS9q6Y5jyVAYsUtg1nDFD5Yz+gVhgXhpJbO2id0sY1pq9wwlQI7Yctbw2zhQvZRurk0Xg3Lz2o+w/s5TM7WBmCJIM/C9lTpgVblfQ3p47YX4gOhlGIYQ3lBkEawI00yZjpVcWomVSHYY13wUs0QIDAQAB'),
-
-    /* 3CX SIP records */
-    SRV('_sip._tcp', 20, 1, 5060, GetHost('Barium')),
-    SRV('_sip._udp', 20, 1, 5060, GetHost('Barium')),
-    SRV('_sip._tls', 20, 1, 5061, GetHost('Barium')),
+    /* Office 365 records */
+    ... CreateOffice365Records('vlad-gg', 'ms43185050'),
+    MX('*', 0, 'vlad-gg.mail.protection.outlook.com.'),
+    CNAME('selector1._domainkey', 'selector1-vlad-gg._domainkey.vladzaharia.onmicrosoft.com.'),
+    CNAME('selector2._domainkey', 'selector2-vlad-gg._domainkey.vladzaharia.onmicrosoft.com.'),
 
     /* Domain verification records */
     TXT('@', 'ybqnhr2z5gddd1kxbgdv6873s7ng47v6')
