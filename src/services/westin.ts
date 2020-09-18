dfcbcffe8ce3414ac3669f02dc5b601d797552c9
Vlad Zaharia <vlad@zaharia.io>
2020-09-18 13:37:54 -0700

    Update westin.ts

--- src/services/westin.ts
+++ src/services/westin.ts
@@ -34,7 +34,7 @@ export const WestinIntServices: Record[] = [
     { name: 'phone', description: '3CX', target: GetHost('Barium') },
     { name: 'tesla', description: 'TeslaMate' }, 
     { name: 'tesla-gf', description: 'TeslaMate - Grafana' }, 
-    { name: 'pritunl', description: 'Pritunl', target: GetHost('Gallium') },
+    { name: 'vpn', description: 'Wireguard', target: GetHost('Gallium') },
 
     /* Warmitup Services */
     { name: 'chat', description: 'Mattermost' },
