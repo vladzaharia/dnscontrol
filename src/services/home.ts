dd324ce5cce809f7e8ec2ae823cc8501e24afbac
Vlad Zaharia <vlad@zaharia.io>
2020-10-15 21:49:58 -0700

    Update home.ts

--- src/services/home.ts
+++ src/services/home.ts
@@ -55,8 +55,10 @@ export const HomeLocalServices: Record[] = [
     /* Rancher k8s */
     { name: 'rancher.dc.local', description: 'Rancher Master', type: 'A', target: '10.0.11.210'},
     { name: 'alpha.rancher.local', description: 'Rancher Node 1', type: 'A', target: '10.0.11.210'},
-    { name: 'beta.rancher.local', description: 'Rancher Node 2', type: 'A', target: '10.0.11.211'},
     { name: 'charlie.rancher.local', description: 'Rancher Node 3', type: 'A', target: '10.0.11.212'},
+    
+    /* VMWare ESXi */
+    { name: 'beta.esxi.local', description: 'ESXi Node', type: 'A', target: '10.0.11.211'},
     
     /* Rancher (internally accessible) k8s services */
     { name: 'traefik.rancher', description: 'Rancher Traefik', type: 'A', target: '10.0.11.210'},
