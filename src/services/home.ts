56855aaad43369516b834de9a29d1c344120bb9e
Vlad Zaharia <vladzaharia@vlads-macbook-pro.local>
2021-01-11 04:02:01 +0000

    Update home services

--- src/services/home.ts
+++ src/services/home.ts
@@ -2,7 +2,7 @@ import { Record } from "../utils/record";
 import { GetHost } from "./core";
 
 /**
- * External (zaharia.io) Home services
+ * External (zaharia.io/zhr.one) Home services
  */
 export const HomeExtServices: Record[] = [
     { name: 'books', description: 'Calibre', proxy: true },
@@ -18,19 +18,7 @@ export const HomeExtServices: Record[] = [
  * Internal (zhr.one) Home services
  */
 export const HomeIntServices: Record[] = [
-    /* Infrastructure */
-    { name: 'proxmox-home', description: 'Home Proxmox' },
-    { name: 'traefik-home', description: 'Home Traefik', proxy: true },
-    { name: 'portainer-home', description: 'Home Portainer', proxy: true },
-
     /* Internal Services */
-    { name: 'ha', description: 'HomeAssistant', proxy: true },
-    { name: 'home', description: 'Home Pritunl VPN' },
     { name: 'parents', description: 'Parents', target: GetHost('Palladium') },
-    { name: 'ping', description: 'Statping' },
-    { name: 'rd', description: 'Guacamole' }, 
-
-    /* Experimental Services */
-    { name: 'sync', description: 'SyncLounge' }, // Added 3/18
-
+    { name: 'pdns', description: 'PowerDNS' }, 
 ];
