5da7e752543671da04c630a78dee80858b32c39c
Vlad Zaharia <vlad@zhr.one>
2021-02-25 00:02:45 -0800

    Fix changed records

--- src/services/home.ts
+++ src/services/home.ts
@@ -5,22 +5,31 @@ import { GetHost } from "./core";
  * External (zaharia.io/zhr.one) Home services
  */
 export const HomeExtServices: Record[] = [
-    { name: 'books', description: 'Calibre', proxy: true },
-    { name: 'deluge', description: 'Deluge', proxy: true },
-    { name: 'ombi', description: 'Ombi' },
-    { name: 'radarr', description: 'Radarr', proxy: true },
-    { name: 'sabnzbd', description: 'SabNZBd', proxy: true },
-    { name: 'sonarr', description: 'Sonarr', proxy: true },
-    { name: 'stats', description: 'Tautulli', proxy: true },
-    { name: 'tp', description: 'Teleport' },
-    { name: 'games', description: 'Pterodactyl' },
 ];
 
 /**
  * Internal (zhr.one) Home services
  */
 export const HomeIntServices: Record[] = [
-    /* Internal Services */
+    /* Infrastructure */
+    { name: 'cockpit', description: 'Cockpit' },
+    { name: 'tp', description: 'Teleport' },
+
+    /* Cloud */
+    { name: 'books', description: 'Calibre' },
+    { name: 'ferdi', description: 'Ferdi' },
+    { name: 'games', description: 'Pterodactyl' },
+    { name: 'print', description: 'OctoPrint' },
+
+    /* Media Pipeline */
+    { name: 'ombi', description: 'Ombi' },
+    { name: 'radarr', description: 'Radarr' },
+    { name: 'sonarr', description: 'Sonarr' },
+
+    /* TeslaMate */
+    { name: 'tesla', description: 'TeslaMate' }, 
+    { name: 'tesla-gf', description: 'TeslaMate - Grafana' }, 
+
+    /* Other Services */
     { name: 'parents', description: 'Parents', target: GetHost('Palladium') },
-    { name: 'pdns', description: 'PowerDNS' }, 
 ];
