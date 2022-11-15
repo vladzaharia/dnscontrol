295a5b02821b6df3c25253de3aa4c1fbd1526f68
Vlad Zaharia <hey@vlad.gg>
2022-11-14 17:03:56 -0800

    Update core.ts

--- src/records/core.ts
+++ src/records/core.ts
@@ -37,7 +37,7 @@ const Servers: ServerMap = {
     description: "Home IP",
     prefix: "he",
     location: "sea",
-    ip: "97.113.219.110",
+    ip: "75.172.19.36",
   },
   Thorium: {
     name: "Thorium",
