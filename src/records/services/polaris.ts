b7a38b3e31a3b5e3e3895dad00513fa17b4d8e69
Vlad Zaharia <hey@vlad.gg>
2022-11-19 21:34:53 -0800

    Update polaris.ts

--- src/records/services/polaris.ts
+++ src/records/services/polaris.ts
@@ -1,11 +1,27 @@
 import { Record } from "../../utils/record";
 
 export const PolarisVideoServices: Record[] = [
-  { name: "ersatz", description: "ErsatzTV" },
-  { name: "iptv", description: "IPTV Service (Primary)" },
-  { name: "media", description: "Plex Media Server" },
-  { name: "player", description: "IPTV Player" },
-  { name: "request", description: "Overseerr Requests" },
-  { name: "xui", description: "IPTV Service (Backup)" },
+  /* Infrastructure */
   { name: "auth", description: "AUthentik - Polaris" },
+  
+  /* Public */
+  { name: "media", description: "Plex Media Server" },
+  { name: "request", description: "Overseerr Requests" },
+  { name: "iptv", description: "IPTV Service (Primary)" },
+  { name: "xui", description: "IPTV Service (Backup)" },
+  { name: "player", description: "IPTV Player" },
+
+  /* Private */
+  { name: "sonarr", description: "Sonarr - TV Indexer" },
+  { name: "radarr", description: "Radarr - Movie Indexer" },
+  { name: "lidarr", description: "Lidarr - Music Indexer" },
+  { name: "readarr", description: "Readerr - Book Indexer" },
+  { name: "prowlarr", description: "Prowlarr - Meta Indexer" },
+ 
+  { name: "sabnzbd", description: "SabNZBd - NZB Downloader" },
+  { name: "transmission", description: "Transmission - Torrent/File Downloader" },
+  { name: "bazarr", description: "Bazarr - Subtitle Downloader" },
+  
+  { name: "tautulli", description: "Tautulli Stats" },
+  { name: "ersatz", description: "ErsatzTV Channels" },
 ];
