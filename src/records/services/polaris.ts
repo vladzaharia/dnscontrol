0b6a500ace128d3c84c394a74b8e3af6bda8e566
Vlad Zaharia <hey@vlad.gg>
2022-12-29 22:08:53 -0800

    Update polaris.ts

--- src/records/services/polaris.ts
+++ src/records/services/polaris.ts
@@ -12,19 +12,19 @@ export const PolarisVideoServices: Record[] = [
   { name: "player", description: "IPTV Player" },
 
   /* Private */
-  { name: "sonarr", description: "Sonarr - TV Indexer" },
-  { name: "radarr", description: "Radarr - Movie Indexer" },
+  { name: "sonarr", description: "Sonarr - TV Indexer", target: "10.12.2.11", type: "A" },
+  { name: "radarr", description: "Radarr - Movie Indexer", target: "10.12.2.12", type: "A" },
   { name: "lidarr", description: "Lidarr - Music Indexer" },
   { name: "readarr", description: "Readerr - Book Indexer" },
-  { name: "prowlarr", description: "Prowlarr - Meta Indexer" },
+  { name: "prowlarr", description: "Prowlarr - Meta Indexer", target: "10.12.2.10", type: "A" },
 
-  { name: "sabnzbd", description: "SabNZBd - NZB Downloader" },
+  { name: "sabnzbd", description: "SabNZBd - NZB Downloader", target: "10.12.2.20", type: "A" },
   {
     name: "transmission",
     description: "Transmission - Torrent/File Downloader",
   },
   { name: "bazarr", description: "Bazarr - Subtitle Downloader" },
 
-  { name: "tautulli", description: "Tautulli Stats" },
+  { name: "tautulli", description: "Tautulli Stats", target: "10.12.2.40", type: "A" },
   { name: "ersatz", description: "ErsatzTV Channels" },
 ];
