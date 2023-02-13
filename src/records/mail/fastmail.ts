95b29b5a96b7e27469605170ad7db2cb025901f0
Vlad Zaharia <hey@vlad.gg>
2023-02-13 03:30:44 +0000

    Clean up fastmail

--- src/records/mail/fastmail.ts
+++ src/records/mail/fastmail.ts
@@ -9,29 +9,26 @@ export function CreateFastmailRecords(
   domain: string,
   rootRecord = "@"
 ): DNSControlRecord[] {
-  const isRoot = rootRecord === "@";
-  const mailRecord = isRoot ? "mail" : `mail.$[domain}`;
-  const wildcardRecord = isRoot ? "*" : `*.${rootRecord}`;
-  const domainKeyRecord = isRoot ? "" : `.${rootRecord}`;
+  const subdomainRecord = rootRecord === "@" ? "" : `.${rootRecord}`;
 
   return [
-    CNAME(mailRecord, "mail.fastmail.com."),
+    CNAME(`mail${subdomainRecord}`, "mail.fastmail.com."),
     MX(rootRecord, 10, "in1-smtp.messagingengine.com."),
     MX(rootRecord, 20, "in2-smtp.messagingengine.com."),
-    MX(wildcardRecord, 10, "in1-smtp.messagingengine.com."),
-    MX(wildcardRecord, 20, "in2-smtp.messagingengine.com."),
+    MX(`*${subdomainRecord}`, 10, "in1-smtp.messagingengine.com."),
+    MX(`*${subdomainRecord}`, 20, "in2-smtp.messagingengine.com."),
     TXT(rootRecord, "v=spf1 include:spf.messagingengine.com ?all"),
-    TXT("_dmarc", "v=DMARC1; p=none"),
+    TXT(`_dmarc${subdomainRecord}`, "v=DMARC1; p=none"),
     CNAME(
-      `fm1._domainkey${domainKeyRecord}`,
+      `fm1._domainkey${subdomainRecord}`,
       `fm1.${domain}.dkim.fmhosted.com.`
     ),
     CNAME(
-      `fm2._domainkey${domainKeyRecord}`,
+      `fm2._domainkey${subdomainRecord}`,
       `fm2.${domain}.dkim.fmhosted.com.`
     ),
     CNAME(
-      `fm3._domainkey${domainKeyRecord}`,
+      `fm3._domainkey${subdomainRecord}`,
       `fm3.${domain}.dkim.fmhosted.com.`
     ),
   ];
