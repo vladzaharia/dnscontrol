46dd2dd4aa3d949d3dedb64e4c3929e0d6b16e43
Vlad Zaharia <hey@vlad.gg>
2023-03-16 23:30:35 -0700

    Update townhouse.ts

--- src/records/locations/townhouse.ts
+++ src/records/locations/townhouse.ts
@@ -7,6 +7,7 @@ export const TownhouseRecords: Record[] = [
   /* Infrastructure */
   { name: "router", description: "Router", target: "10.10.0.1", type: "A" },
   { name: "truenas", description: "TrueNAS", target: "10.10.0.10", type: "A" },
+  { name: "traefik", description: "Traefik" },
   { name: "auth", description: "Authentik" },
   { name: "vault", description: "Hashicorp Vault" },
   { name: "env", description: "Infisical" },
