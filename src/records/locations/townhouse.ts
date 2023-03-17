7e925639463fcc63d3981f43bf872b6eee6606dc
Vlad Zaharia <hey@vlad.gg>
2023-03-16 17:05:44 -0700

    Update townhouse.ts
    Clean up townhouse services

--- src/records/locations/townhouse.ts
+++ src/records/locations/townhouse.ts
@@ -14,30 +14,14 @@ export const TownhouseRecords: Record[] = [
     type: "A",
   },
   { name: "auth", description: "Authentik" },
-  { name: "guac", description: "Guacamole" },
-  { name: "homevpn", description: "Subspace VPN" },
-  {
-    name: "hs",
-    description: "Headscale",
-    target: "zhrone-hs.westus2.cloudapp.azure.com.",
-  },
-  { name: "s3", description: "Minio S3" },
-  { name: "s3c", description: "Minio S3" },
-  { name: "artifacts", description: "Nexus-OSS" },
-  { name: "docker", description: "Docker Registry" },
+  { name: "hashicorp", description: "Hashicorp" },
   { name: "env", description: "Infisical" },
 
   /* Personal Services */
-  { name: "asset", description: "Snipe-It" },
   { name: "cloud", description: "Nextcloud" },
   { name: "docs", description: "Collabora" },
-  { name: "hb", description: "HOOBS" },
-  { name: "kasm", description: "KASM" },
+  { name: "kasm", description: "Kasm" },
   { name: "kiwix", description: "Kiwix", target: "10.11.2.123", type: "A" },
-  { name: "ml", description: "Machine Learning", proxy: true },
-  { name: "outline", description: "Outline" },
-  { name: "urbit", description: "Urbit" },
-  { name: "youtrack", description: "YouTrack" },
 
   /* Smart Home */
   { name: "hubitat", description: "Hubitat" },
@@ -46,10 +30,5 @@ export const TownhouseRecords: Record[] = [
 
   /* Personal Media Services */
   { name: "books", description: "Calibre" },
-  { name: "music", description: "Airsonic" },
   { name: "stash", description: "Stash" },
-  { name: "pod", description: "PodGrab Podcasts" },
-
-  /* TeslaMate */
-  { name: "tesla", description: "TeslaMate" },
 ];
