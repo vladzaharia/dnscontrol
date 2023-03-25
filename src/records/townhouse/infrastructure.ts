5136e3177698f2c628e6de5ccc466091d1059620
Vlad Zaharia <hey@vlad.gg>
2023-03-25 00:42:40 -0700

    Add AdGuard internal record

--- src/records/townhouse/infrastructure.ts
+++ src/records/townhouse/infrastructure.ts
@@ -7,6 +7,7 @@ export const InfrastructureRecords: Record[] = [
   /* Infrastructure */
   { name: "router", description: "Router", target: "10.10.0.1", type: "A" },
   { name: "truenas", description: "TrueNAS", target: "10.10.0.10", type: "A" },
+  { name: "adguard", description: "Adguard", target: "10.10.2.5", type: "A" },
   { name: "traefik", description: "Traefik" },
   { name: "auth", description: "Authentik" },
   { name: "vault", description: "Hashicorp Vault" },
