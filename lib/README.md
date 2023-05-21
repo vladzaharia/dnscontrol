655d6b32e4872fdd191d108a80005df792cabaf8
Vlad Zaharia <hey@vlad.gg>
2023-05-21 07:32:13 -0700

    Switch to V4 of dnscontrol

--- lib/README.md
+++
@@ -1,3 +0,0 @@
-# Downloading DNSControl
-
-Execute either `download.sh` or `download.ps1` to download the latest binaries.
--- lib/download.sh
+++ lib/download.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 BASE_DIR=$(dirname "$0")
+DL_DIR="$BASE_DIR/dl"
 
 # Determine latest version
 GITHUB_BASE_URL="https://github.com/StackExchange/dnscontrol/releases"
-# LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' $GITHUB_BASE_URL/latest)
-# LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-LATEST_VERSION="v3.31.4"
-ARTIFACT_BASE_URL="$GITHUB_BASE_URL/download/$LATEST_VERSION"
+RELEASE=$(curl -L -s -H 'Accept: application/json' $GITHUB_BASE_URL/latest)
+VERSION=$(echo $RELEASE | sed -e 's/.*"tag_name":"v\([^"]*\)".*/\1/')
+ARTIFACT_BASE_URL="$GITHUB_BASE_URL/download/v$VERSION"
 
 # Download executables
 if [ ! -f "$BASE_DIR/dnscontrol-Darwin" ]; then
-wget -O $BASE_DIR/dnscontrol-Darwin $ARTIFACT_BASE_URL/dnscontrol-Darwin
+  mkdir -p $DL_DIR
+  wget -O $DL_DIR/dnscontrol_$VERSION\_darwin_all.tar.gz $ARTIFACT_BASE_URL/dnscontrol_$VERSION\_darwin_all.tar.gz
+  tar -xvf $DL_DIR/dnscontrol_$VERSION\_darwin_all.tar.gz -C $DL_DIR
+  mv $DL_DIR/dnscontrol $BASE_DIR/dnscontrol-Darwin
+  rm -rf $DL_DIR
 fi
 
 if [ ! -f "$BASE_DIR/dnscontrol-Linux" ]; then
 wget -O $BASE_DIR/dnscontrol-Linux $ARTIFACT_BASE_URL/dnscontrol-Linux
-fi
-
-if [ ! -f "$BASE_DIR/dnscontrol.exe" ]; then
-wget -O $BASE_DIR/dnscontrol.exe $ARTIFACT_BASE_URL/dnscontrol.exe
+  mkdir -p $DL_DIR
+  wget -O $DL_DIR/dnscontrol_$VERSION\_linux_amd64.tar.gz $ARTIFACT_BASE_URL/dnscontrol_$VERSION\_linux_amd64.tar.gz
+  tar -xvf $DL_DIR/dnscontrol_$VERSION\_linux_amd64.tar.gz -C $DL_DIR
+  mv $DL_DIR/dnscontrol $BASE_DIR/dnscontrol-Linux
+  rm -rf $DL_DIR
 fi
 
 chmod +x $BASE_DIR/dnscontrol*
 
 # Print out latest version
-echo $LATEST_VERSION > $BASE_DIR/dnscontrol-VERSION
+echo $VERSION > $BASE_DIR/dnscontrol-VERSION
