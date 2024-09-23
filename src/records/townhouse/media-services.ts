70a97fdfb3d71da00ac214e38e1c42b2c312fdce
Vlad Zaharia <hey@vlad.gg>
2024-09-23 14:17:23 -0700

    Update media-services.ts

--- src/records/townhouse/media-services.ts
+++ src/records/townhouse/media-services.ts
@@ -6,5 +6,6 @@ import { Record } from "../../utils/record";
 export const MediaServiceRecords: Record[] = [
   { name: "books", description: "Calibre Web" },
   { name: "calibre", description: "Calibre" },
+  { name: "manga", description: "Komga" },
   { name: "stash", description: "Stash" },
 ];
